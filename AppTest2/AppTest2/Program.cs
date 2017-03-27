e869fe85407dc37142122c9adf0f9aeed609ead3
theOwls <woodliam003@gmail.com>
2017-03-27 16:03:31 +0100

    2 rooms, player and controller class
    The controller class now has responsibility for creating the game world. Right now that consists of a player and 2 rooms.

---
+++ AppTest2/AppTest2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTest2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Controller start = new Controller();
+
+        }
+    }
+}
---
+++ AppTest2/AppTest2/Sprite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTest2
+{
+    class Sprite
+    {
+        private int currentRoom;
+
+        public Sprite()
+        {
+            this.currentRoom = 1;
+        }
+
+        public void ChangeCoordinates(int nextRoom)
+        {
+            this.currentRoom = int.Parse(Console.ReadLine());
+            Console.WriteLine("You are now in room {0}", this.currentRoom);
+        }
+    }
+}
